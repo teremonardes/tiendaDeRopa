22dc0afcaeb544e6aa2f7d4aed047734b1c400c5
Teresita Monardes <teremonardes.s@gmail.com>
2025-05-29 18:08:11 -0400

    Revert "Login"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 // import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-<<<<<<< HEAD
 import ProductProvider from './components/Context/fetchContext'
-=======
->>>>>>> 7750dfa (login)
 
 import NavbarT from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
@@ -11,10 +8,6 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import NotFound from './pages/NotFound/NotFound' 
 import Footer from './components/Footer/Footer'
-<<<<<<< HEAD
-=======
-import Login from './pages/Login/Login.jsx';
->>>>>>> 7750dfa (login)
 
 
 
@@ -27,29 +20,19 @@ function App() {
 
   return (
   <BrowserRouter>
-<<<<<<< HEAD
   <ProductProvider>
-=======
->>>>>>> 7750dfa (login)
   <Header />
   <NavbarT />
   <Routes>
      <Route path="/" element={<Home />} />
      <Route path="/cart" element={<Cart />} />
      {/* path='gallery/:product/:id' */}
-<<<<<<< HEAD
-=======
-     <Route path="/login" element={<Login />} />
->>>>>>> 7750dfa (login)
      <Route path="*" element={<NotFound />} />
 
   </Routes>
 
 <Footer />
-<<<<<<< HEAD
 </ProductProvider>
-=======
->>>>>>> 7750dfa (login)
  </BrowserRouter>
   )
 }
