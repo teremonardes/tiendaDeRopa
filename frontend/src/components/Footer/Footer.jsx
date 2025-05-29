7776e1ab9d638d29ea6eab5de92d0ca72d115779
Tere Monardes <teremonardes.s@gmail.com>
2025-05-28 20:55:12 -0400

    footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+
+const Footer = () => {
+  return (
+       <p className='page-footer text-light text-center fixed-bottom mb-0' style={{ backgroundColor: '#DA8383' }} > 2025 - Tienda Mi Closet - Todos los derechos reservados
+    </p>
+  )
+}
+
+export default Footer
