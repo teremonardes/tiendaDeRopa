4224dcd6e0dde5601c871554760e35d3b665ac19
Tere Monardes <teremonardes.s@gmail.com>
2025-06-02 16:33:50 -0400

    detalleproducto

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+
 
 import App from './App.jsx'
-import { CartProvider } from './components/Context/cartContext'
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
-  </StrictMode>
+         <App />
+   </StrictMode>
 )
