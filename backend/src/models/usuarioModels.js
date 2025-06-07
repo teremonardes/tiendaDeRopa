7d34578f2054f49f7d3869a05b1f3628b843bd73
Tere Monardes <teremonardes.s@gmail.com>
2025-06-07 13:52:01 -0400

    union de los register login duplicados

--- backend/src/models/usuarioModels.js
+++ backend/src/models/usuarioModels.js
@@ -1,8 +1,17 @@
 import db from '../../bd/config.js'
+import bycrypt from 'bcrypt'
 
-export const registrarUsuario = async ({ nombre, email, password }) => {
+export const registroUsuario = async ({ nombre, mail, pass }) => {
+  const hashedPassword = bycrypt.hashSync(pass, 10)
   const query = 'INSERT INTO usuario (nombre, email, password) VALUES ($1, $2, $3) RETURNING *'
-  const values = [nombre, email, password]
+  const values = [nombre, mail, hashedPassword]
+  const result = await db.query(query, values)
+  return result.rows[0]
+}
+
+export const obtenerUsuarioPorEmail = async (mail) => {
+  const query = 'SELECT * FROM usuario WHERE mail = $1'
+  const values = [mail]
   const result = await db.query(query, values)
   return result.rows[0]
 }
